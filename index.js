e607e24c9cc678b81170e4c26b6fc9f7ca6ea8f2
Ganesham <ganeshamtailor@gmail.com>
2023-05-16 09:55:42 +0530

    changes for express initial setup

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
-
+const bodyParser= require('body-parser')
 const app = express()
 
-app.listen('3000', ()=>{
+app.use(bodyParser.json())
+
+app.get('/hello-world',(req, res) => {
+    res.send('hello world')
+})
+
+app.post('/new-request',(req,res)=>{
+    res.send(req.body)
+})
+
+app.listen('3300', ()=>{
     console.log('listen at 3000')
 })
